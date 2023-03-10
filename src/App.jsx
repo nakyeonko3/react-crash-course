f537bc28e21b309a080e255d2057131850b3af48
nakyeonko3 <badaclock@gmail.com>
2023-03-10 21:36:43 +0900

    new repo commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+    </div>
+  );
+}
+
+export default App;
